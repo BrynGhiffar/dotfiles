5e304ea62e81354f7b72c3bf03e370bd3476ae8d
Bryn Ghiffar <bryn.ghiffar@gmail.com>
2025-09-17 13:06:36 +0700

    update nvim config

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -1,5 +1,18 @@
 return {
 	{
-		"lewis6991/gitsigns.nvim"
+		"lewis6991/gitsigns.nvim",
+		config = function()
+			require('gitsigns').setup({
+				current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
+  				current_line_blame_opts = {
+					virt_text = true,
+					virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
+					delay = 1000,
+					ignore_whitespace = false,
+					virt_text_priority = 100,
+					use_focus = true,
+  				},
+			})
+		end
 	}
 }
